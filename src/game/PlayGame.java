621fa9dd4a0cf976928b808906f38bd4af33d214
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2023-02-15 01:13:38 -0800

    added instructions to PlayGame class
    informs user how to run application

--- src/game/PlayGame.java
+++ src/game/PlayGame.java
@@ -1,5 +1,7 @@
 package game;
 
+//to run the game open command line and go to src directory
+//and type this command: java ./game/PlayGame.java
 public class PlayGame {
 
 	public static void main(String[] args) {
